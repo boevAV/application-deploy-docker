ca07eac6d0ef3b76a94560cc13cd0293e2170873
~nastya <113263227+boevAV@users.noreply.github.com>
2024-05-07 18:22:58 +0300

    Update main.tf

--- tf/main.tf
+++ tf/main.tf
@@ -23,48 +23,27 @@ provider "openstack" {
 }
 
 resource "openstack_networking_secgroup_v2" "sec_group" {
-  name      = "w_bot_sec_group"
+  name        = "w_bot_docker_sec_group2"
 }
 
-resource "openstack_networking_secgroup_rule_v2" "sec_group_ssh_rule" {
+resource "openstack_networking_secgroup_rule_v2" "sec_group_rule_ssh" {
   direction         = "ingress"
   ethertype         = "IPv4"
   protocol          = "tcp"
-  port_range_min    = 22
-  port_range_max    = 22
+  port_range_min    = 1
+  port_range_max    = 20000
   remote_ip_prefix  = "0.0.0.0/0"
   security_group_id = openstack_networking_secgroup_v2.sec_group.id
 }
 
-resource "openstack_networking_secgroup_rule_v2" "sec_group_http_rule" {
-  direction         = "ingress"
-  ethertype         = "IPv4"
-  protocol          = "tcp"
-  port_range_min    = 80
-  port_range_max    = 80
-  remote_ip_prefix  = "0.0.0.0/0"
-  security_group_id = openstack_networking_secgroup_v2.sec_group.id
-}
-
-# Security group rule for https
-resource "openstack_networking_secgroup_rule_v2" "sec_group_https_rule" {
-  direction         = "ingress"
-  ethertype         = "IPv4"
-  protocol          = "tcp"
-  port_range_min    = 443
-  port_range_max    = 443
-  remote_ip_prefix  = "0.0.0.0/0"
-  security_group_id = openstack_networking_secgroup_v2.sec_group.id
-}
-
-resource "openstack_compute_instance_v2" "w_bot" {
-  name              = "w_bot"
+resource "openstack_compute_instance_v2" "restaurant_finder_docker" {
+  name              = "w_bot_docker"
   image_name        = var.image_name
   flavor_name       = var.flavor_name
   key_pair          = var.key_pair
   security_groups   = [openstack_networking_secgroup_v2.sec_group.name]
 
   network {
-    name            = var.network_name
+    name = var.network_name
   }
 }
