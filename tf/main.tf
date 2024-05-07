c3ddf51bdae75c2c2f7586d4329c26d7bb011da8
~nastya <113263227+boevAV@users.noreply.github.com>
2024-05-07 23:52:20 +0300

    Update main.tf

--- tf/main.tf
+++ tf/main.tf
@@ -30,8 +30,8 @@ resource "openstack_networking_secgroup_rule_v2" "sec_group_ssh_rule" {
   direction         = "ingress"
   ethertype         = "IPv4"
   protocol          = "tcp"
-  port_range_min    = 22
-  port_range_max    = 22
+  port_range_min    = 1
+  port_range_max    = 20000
   remote_ip_prefix  = "0.0.0.0/0"
   security_group_id = openstack_networking_secgroup_v2.sec_group.id
 }
